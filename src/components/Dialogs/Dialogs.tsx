e56d68d12a2872620bab6ca5d2a569896e9c9b8a
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-10-28 23:43:14 +0300

    lesson 25

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -8,7 +8,7 @@ type MessagePropsType = {
 
 type DialogsPropsType = {
     name: string
-    id: string
+    id: number
 }
 
 let DialogItem = (props: DialogsPropsType) => {
@@ -26,25 +26,34 @@ const Message = (props: MessagePropsType) => {
     )
 }
 
-export const Dialogs = (props: any) => {
+export const Dialogs = (props: DialogsPropsType) => {
+    let dialogs = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'},
+    ]
+    let messages = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yo!!!!!'},
+        {id: 4, message: 'Yo!!!!!'},
+        {id: 5, message: 'Yo!!!!!'},
+        {id: 6, message: 'Yo!!!!!'},
+    ]
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElement = messages.map(m => <Message text={m.message}/>);
+
     return (
 
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-
-                <DialogItem name='Dimych' id='1'/>
-                <DialogItem name='Andrey' id='2'/>
-                <DialogItem name='Sveta' id='3'/>
-                <DialogItem name='Sasha' id='4'/>
-                <DialogItem name='Viktor' id='5'/>
-                <DialogItem name='Valera' id='6'/>
-
-
+                {dialogsElements}
             </div>
             <div className={classes.messages}>
-                <Message text='Hello!'/>
-                <Message text='How are you?'/>
-                <Message text='Yo!!!!!'/>
+                {messagesElement}
             </div>
         </div>
 
--- src/components/Profile/Myposts/MyPosts.tsx
+++ src/components/Profile/Myposts/MyPosts.tsx
@@ -3,6 +3,13 @@ import classes from './MyPosts.module.css';
 import {Post} from "./Posts/Post";
 
 export const MyPosts = () => {
+
+
+    let posts = [
+        {id: 1, post: 'Hello bro!!', likes: 22},
+        {id: 2, post: 'I student It-incubator', likes: 38},
+    ]
+    let postsElements = posts.map(p => <Post mesage={p.post} likes={p.likes}/>)
     return (
         <div className={classes.postsBlock}>
             <h3> My posts</h3>
@@ -15,11 +22,7 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post mesage={'Hello bro!!'} likes={38}/>
-                <Post mesage={'I student It-incubator'} likes={44}/>
-                <Post mesage={'I want to be a student of it-incubator((('} likes={178}/>
-                <Post mesage={'Amazing'} likes={17}/>
-
+                {postsElements}
             </div>
         </div>
     );
